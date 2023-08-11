1d76f218b3e426c1f7fb8e4fb2b7aa3c806df519
Mizar <122654099+Nordlicht215@users.noreply.github.com>
2023-08-11 17:23:26 -0600

    cosas de junior jsjsj

---
+++ junior/main.js
@@ -0,0 +1,10 @@
+function showhide(id){
+    let div=document.getElementById(id);
+    if (div.style.display=="none"){
+        div.style.display="flex";
+
+    }
+    else{
+        div.style.display="none";
+    }
+}
